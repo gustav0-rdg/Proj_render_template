833f1acc621bd168e618c52be3717acd8e5517f0
gustavo <gustavolourenco821@gmail.com>
2025-01-28 08:13:16 -0300

    correcao de pastas e base do python

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ import random
 
 app = Flask(__name__)
 
+# Listas de nomes para alterar no html
+
 nomes_lista = ["Godo", "Alex", "Ivo"]
+
+# Aqui irá todas minhas rotas
+
 @app.route("/")
 def pagina_inicial():
     nome = random.choice(nomes_lista)
-    return render_template('index.html', nome = nome)
+    return render_template('index.html')
 
+# Fazer o app rodar
 app.run(debug=True, host="0.0.0.0", port=8080)
